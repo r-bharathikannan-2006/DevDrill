64072e95e40cb89e1381c4f905db318b5d75cc42
r-bharathikannan-2006 <r.bharathikannan.official@gmail.com>
2025-09-21 19:48:57 +0530

    Update settings.py

--- youtube_quiz/settings.py
+++ youtube_quiz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1)=*h)pi4$wbc$@b2=#)&5#n&7moxtn=gf6qyi2+%fs72qfwg3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://devdrill.onrender.com',]
 
 
 # Application definition
@@ -121,3 +121,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
