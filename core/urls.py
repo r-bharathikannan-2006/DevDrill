37c14937740cf61784cd218e3bd480557aa8b6f4
r-bharathikannan-2006 <r.bharathikannan.official@gmail.com>
2025-09-21 19:20:14 +0530

    Delete core directory

--- core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.search_template, name='search_template'),
-    path('player/<str:video_id>/', views.open_player, name='open_player'),
-]
